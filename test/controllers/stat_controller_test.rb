dab340e0e37874d3b6dcc447121dcbf33c524f88
Alex <holyketzer@gmail.com>
2013-11-15 12:59:04 +0400

    Fixed tests.

--- test/controllers/stat_controller_test.rb
+++ test/controllers/stat_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class StatControllerTest < ActionController::TestCase
-  test "should get time" do
-    get :time
+  test "should get vacancy count by month in" do
+    get :count_by_month_in, :year => 2013
+    assert_response :success
+  end
+
+  test "should get salary distribution in" do
+    get :salary_distribution_in, :year => 2013
+    assert_response :success
+  end
+
+  test "should get vacancy count by year" do
+    get :count_by_year
+    assert_response :success
+  end
+
+  test "should get vacancy count by month" do
+    get :count_by_month
     assert_response :success
   end
 
