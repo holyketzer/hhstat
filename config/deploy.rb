fde347dc51a49136d89a2422f908f535a9b1707b
Alex Emelyanov <holyketzer@gmail.com>
2013-12-06 11:30:58 +0400

    Capistrano runs bundle install

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,9 @@
 # For deploy use parameter -S domain=user@host:port, for example
 # cap deploy -S domain=deployer@youdomain.com:22
 
+require "bundler/capistrano"
+require 'rvm/capistrano'
+
 set :application, "hhstat"
 set :repository,  "https://github.com/holyketzer/hhstat.git"
 set :deploy_to, "/var/www/hhstat"
@@ -12,7 +15,6 @@ puts "Wash! Rinse! Repeat!"
 # adjust if you are using RVM, remove if you are not
 set :rvm_type, :user
 set :rvm_ruby_string, 'ruby-2.0.0-p247'
-require 'rvm/capistrano'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
