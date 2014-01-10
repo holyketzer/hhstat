835f0a5b6e6bb107c8dba19167ad15d864058323
Alex <holyketzer@gmail.com>
2014-01-10 23:34:38 +0400

    Add page caching for trend pages

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -1,7 +1,7 @@
 class StatController < ApplicationController
   include StatHelper
 
-  caches_page :count_by_month_in, :salary_distribution_in, :count_by_year, :count_by_month, :count_by_specialization, :count_by_class, :mean_salary_by_specialization, :salary_distribution_in_for
+  caches_page :count_by_month_in, :salary_distribution_in, :count_by_year, :count_by_month, :count_by_specialization, :count_by_class, :mean_salary_by_specialization, :salary_distribution_in_for, :specialization_trend_in_for, :specialization_trend_for
 
   before_filter do
     @year = params.has_key?(:year) ? params[:year].to_i : Date.today.year
